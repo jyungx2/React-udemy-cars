15bb31ec36c77fc29e8649c59ad5841d2910668a
jiyoungnim <cloundyon31@gmail.com>
2024-12-03 22:53:49 +0900

    🚧 345. Total Car Cost

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,5 +1,23 @@
+import { useSelector } from "react-redux";
+
 function CarValue() {
-  return <div>CarValue</div>;
+  const totalCost = useSelector(
+    ({ cars: { data, searchTerm } }) =>
+      data
+        .filter((car) =>
+          car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        .reduce((acc, car) => acc + car.cost, 0)
+
+    // Total Car Cost
+    // ✅ 다음 for-of loop대신에 위의 reducer() 메소드 이용
+    // let cost = 0;
+    // for (let car of filteredCars) {
+    //   cost += car.cost;
+    // }
+    // return cost;
+  );
+  return <div className="car-value">Total Cost: ${totalCost}</div>;
 }
 
 export default CarValue;
